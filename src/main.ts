6cab4dd4ab50f334ad132365e23c09e81890e5be
Toma Rosa <toma.rosa@protonmail.com>
2024-01-17 14:10:08 +0100

    04

--- src/main.ts
+++ src/main.ts
@@ -14,17 +14,41 @@ const sizes = {
 const scene = new t.Scene();
 
 // Object
-const geometry = new t.BoxGeometry(1, 1, 1);
-const material = new t.MeshBasicMaterial({
-  color: "blue",
-});
-const mesh = new t.Mesh(geometry, material);
-scene.add(mesh);
+const group = new t.Group();
+scene.add(group);
+
+const cube1 = new t.Mesh(
+  new t.BoxGeometry(1, 1, 1),
+  new t.MeshBasicMaterial({ color: "cyan" })
+);
+group.add(cube1);
+
+const cube2 = new t.Mesh(
+  new t.BoxGeometry(1, 1, 1),
+  new t.MeshBasicMaterial({ color: "red" })
+);
+group.add(cube2);
+
+cube2.position.x = -2;
+
+const cube3 = new t.Mesh(
+  new t.BoxGeometry(1, 1, 1),
+  new t.MeshBasicMaterial({ color: "green" })
+);
+group.add(cube3);
+
+cube3.position.x = 2;
 
 // Camera
 const camera = new t.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
 scene.add(camera);
+
+camera.position.set(0, 5, 3);
+camera.lookAt(group.position);
+
+// Helper
+const axesHelper = new t.AxesHelper(2);
+scene.add(axesHelper);
 
 // Renderer
 const renderer = new t.WebGLRenderer({
