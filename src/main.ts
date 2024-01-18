b4c08c9a41d8f094b8367fd8ee6d0bed3b5e9cc4
Toma Rosa <toma.rosa@protonmail.com>
2024-01-18 10:43:37 +0100

    06

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,94 @@
-import gsap from "gsap";
+import "./index.css";
 import * as t from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
 
 // Canvas
 const canvas = t.createCanvasElement();
 document.querySelector("#app")?.appendChild(canvas);
 
+canvas.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height) - 0.5;
+});
+
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
+
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+  const fullscreenElement = document.fullscreenElement;
+
+  if (!fullscreenElement) {
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
 
 // Scene
 const scene = new t.Scene();
 
-//Camera
-const camera = new t.PerspectiveCamera(75, sizes.width / sizes.height);
+// Camera
+const camera = new t.PerspectiveCamera(
+  60,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
 scene.add(camera);
-
 camera.position.set(0, 0, 3);
 
-// Group
-const cubes = new t.Group();
-scene.add(cubes);
-
-// Objects
-const cube1 = new t.Mesh(
+// Object
+const cube = new t.Mesh(
   new t.BoxGeometry(1, 1, 1),
-  new t.MeshBasicMaterial({ color: "red" })
+  new t.MeshBasicMaterial({ color: "red" }),
 );
+scene.add(cube);
 
-cubes.add(cube1);
-
-//Helper
+// Helper
 const axesHelper = new t.AxesHelper();
 scene.add(axesHelper);
 
-// Renderer
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Render
 const renderer = new t.WebGLRenderer({
   canvas,
 });
+
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-gsap.to(camera.position, { duration: 1, x: 2, delay: 1 });
-
-// Clock
-// const clock = new t.Clock();
-
-// Animation
 function tick() {
-  camera.lookAt(cubes.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 4;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 4;
+  // camera.lookAt(cube.position);
+  // camera.position.y = cursor.y;
 
-  // Clock
-  // const elapsedTime = clock.getElapsedTime();
-
-  // Update Object
-  // camera.position.y = Math.sin(elapsedTime);
-  // camera.position.x = Math.cos(elapsedTime);
-
-  // Render
   renderer.render(scene, camera);
-
-  // Loop
+  controls.update();
   window.requestAnimationFrame(tick);
 }
 
