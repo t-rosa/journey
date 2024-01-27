624039f7bd79ffabb87c5601a3c77ea356c1e05b
Toma Rosa <toma.rosa@protonmail.com>
2024-01-27 22:53:26 +0100

    12

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import gsap from "gsap";
 import GUI from "lil-gui";
 import * as t from "three";
-import { OrbitControls, RGBELoader } from "three/examples/jsm/Addons.js";
+import {
+  FontLoader,
+  OrbitControls,
+  TextGeometry,
+} from "three/examples/jsm/Addons.js";
 import "./index.css";
 
 //Debug
-const gui = new GUI()
+const gui = new GUI();
 
 // canvas
 const canvas = t.createCanvasElement();
@@ -38,41 +42,55 @@ window.addEventListener("dblclick", () => {
 // Scene
 const scene = new t.Scene();
 
-// Lights
-// const ambientLight = new t.AmbientLight("white", 1);
-// scene.add(ambientLight);
-
-// const pointLight = new t.PointLight("white", 30);
-// pointLight.position.set(2, 3, 4);
-// scene.add(pointLight);
-
-// Environment map
-const rgbeLoader = new RGBELoader()
-rgbeLoader.load('/textures/environmentMap/2k.hdr', (environmentMap) => {
-  environmentMap.mapping = t.EquirectangularReflectionMapping
-  scene.background = environmentMap
-  scene.environment = environmentMap
-})
-
-// textures
-const loadingManager = new t.LoadingManager();
-const textureLoader = new t.TextureLoader(loadingManager);
-
-const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
-const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
-const doorAmbientOcclusionTexture = textureLoader.load(
-  "/textures/door/ambientOcclusion.jpg"
-);
-const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
-const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
-const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
-const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
-const matcapTexture = textureLoader.load("/textures/matcaps/8.png");
-const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
-
-doorColorTexture.colorSpace = t.SRGBColorSpace;
+const textureLoader = new t.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/4.png");
 matcapTexture.colorSpace = t.SRGBColorSpace;
 
+// Fonts
+const fontLoader = new FontLoader();
+fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
+  const textGeometry = new TextGeometry("Toma & Maureen\n           <3", {
+    font,
+    size: 0.5,
+    height: 0.1,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 3,
+  });
+
+  const textMaterial = new t.MeshMatcapMaterial({ matcap: matcapTexture });
+  const text = new t.Mesh(textGeometry, textMaterial);
+  textGeometry.computeBoundingBox();
+  // textGeometry.translate(
+  //   -(textGeometry.boundingBox!.max.x - 0.02) / 2,
+  //   0,
+  //   -(textGeometry.boundingBox!.max.z - 0.02) / 2
+  // );
+  textGeometry.center();
+  scene.add(text);
+
+  const geometry = new t.TorusGeometry();
+  const material = new t.MeshMatcapMaterial({ matcap: matcapTexture });
+
+  for (let i = 0; i < 100; i++) {
+    const donut = new t.Mesh(geometry, material);
+
+    gsap.to(donut.position, { x: (Math.random() - 0.5) * 20, duration: 1 });
+    gsap.to(donut.position, { y: (Math.random() - 0.5) * 20, duration: 1 });
+    gsap.to(donut.position, { z: (Math.random() - 0.5) * 20, duration: 1 });
+
+    gsap.to(donut.rotation, { x: Math.random() * Math.PI, duration: 1.6 });
+    gsap.to(donut.rotation, { y: Math.random() * Math.PI, duration: 1.6 });
+
+    const scale = Math.random();
+    donut.scale.set(scale, scale, scale);
+
+    scene.add(donut);
+  }
+});
 
 // Camera
 const camera = new t.PerspectiveCamera(
@@ -81,73 +99,8 @@ const camera = new t.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 5;
+camera.position.z = 10;
 scene.add(camera);
-
-// Meshes
-
-// const material = new t.MeshBasicMaterial();
-// material.map = doorColorTexture;
-// material.color = new t.Color("red");
-// material.side = t.DoubleSide;
-
-// const material = new t.MeshNormalMaterial();
-// material.flatShading = true;
-
-// const material = new t.MeshStandardMaterial();
-// material.map = doorColorTexture
-// material.aoMap = doorAmbientOcclusionTexture
-// material.side = t.DoubleSide
-// material.aoMapIntensity = 1
-// material.displacementMap = doorHeightTexture
-// material.displacementScale = 0.01
-// material.metalnessMap = doorMetalnessTexture
-// material.roughnessMap = doorRoughnessTexture
-// material.normalMap = doorNormalTexture
-// material.transparent = true
-// material.alphaMap = doorAlphaTexture
-
-const material = new t.MeshPhysicalMaterial();
-material.map = doorColorTexture
-material.aoMap = doorAmbientOcclusionTexture
-material.side = t.DoubleSide
-material.aoMapIntensity = 1
-material.displacementMap = doorHeightTexture
-material.displacementScale = 0.01
-material.metalnessMap = doorMetalnessTexture
-material.roughnessMap = doorRoughnessTexture
-material.normalMap = doorNormalTexture
-material.transparent = true
-material.alphaMap = doorAlphaTexture
-
-gui.add(material, 'metalness').min(0).max(1).step(0.0001)
-gui.add(material, 'roughness').min(0).max(1).step(0.0001)
-gui.add(material, 'displacementScale').min(0).max(1).step(0.01)
-gui.add(material, 'aoMapIntensity').min(1).max(10).step(1)
-
-// material.clearcoat = 1
-// material.clearcoatRoughness = 0
-
-// gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
-// gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001)
-
-material.sheen = 1
-material.sheenRoughness =0.25 
-material.sheenColor.set(1,1,1)
-
-gui.add(material, 'sheen').min(0).max(1).step(0.0001)
-gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
-gui.addColor(material, 'sheenColor')
-
-const sphere = new t.Mesh(new t.SphereGeometry(0.5, 64, 64), material);
-sphere.position.x = -1.5;
-
-const plane = new t.Mesh(new t.PlaneGeometry(1, 1, 100,100), material);
-
-const torus = new t.Mesh(new t.TorusGeometry(0.3, 0.2, 64, 128), material);
-torus.position.x = 1.5;
-
-scene.add(sphere, plane, torus);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
@@ -161,15 +114,6 @@ renderer.render(scene, camera);
 renderer.setSize(sizes.width, sizes.height);
 
 function tick() {
-  gsap.to(sphere.rotation, { y: sphere.rotation.y + Math.PI * 0.03 });
-  gsap.to(sphere.rotation, { x: sphere.rotation.x + Math.PI * -0.03 });
-
-  gsap.to(plane.rotation, { y: plane.rotation.y + Math.PI * 0.03 });
-  gsap.to(plane.rotation, { x: plane.rotation.x + Math.PI * -0.03 });
-
-  gsap.to(torus.rotation, { y: torus.rotation.y + Math.PI * 0.03 });
-  gsap.to(torus.rotation, { x: torus.rotation.x + Math.PI * -0.03 });
-
   controls.update();
 
   renderer.render(scene, camera);
